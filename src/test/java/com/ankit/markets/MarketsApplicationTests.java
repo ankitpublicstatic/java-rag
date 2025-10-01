a0fd7e6e25139947149670d091711802202880dc
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-01 19:02:03 +0530

    Initial commit

---
+++ src/test/java/com/ankit/markets/MarketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.markets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
